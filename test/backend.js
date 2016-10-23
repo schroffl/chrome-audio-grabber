8e8525a9fd49a1518833ba7e5f5981583c0fc81f
Hannes <Hannes.Holzwarth@me.com>
2016-10-23 14:31:23 +0200

    Interleaving audio data in order to use multiple channels (required by speaker module)

--- test/backend.js
+++ test/backend.js
@@ -1,28 +1,42 @@
 'use strict';
 
+const url = require('url');
+const qs = require('qs');
+
 const WebSocketServer = require('ws').Server;
 const wss = new WebSocketServer({ 'port': 1337 });
 
 const Speaker = require('speaker');
 
 wss.on('connection', sock => {
-	let numChannels,
-		speaker;
+	let hsData = qs.parse( url.parse(sock.upgradeReq.url).query ),
+		numChannels = hsData.numChannels,
+		bufferSize = hsData.bufferSize;
+
+	let speaker = new Speaker({
+		'channels': hsData.numChannels,
+		'sampleRate': 48000
+	});
 
 	console.log('Client connected');
 
 	sock.on('message', data => {
-		if(!speaker || !numChannels) {
-			numChannels = data;
+		let channels = [ ], audioData = new Buffer(data.length), // Buffer.allocUnsafe is probably better for node version > 6
+			chunkSize = bufferSize * 2;
+ 	
+		for(let i=0; i<data.length; i+=chunkSize)
+			channels.push(data.slice(i, i + chunkSize));
 
-			speaker = new Speaker({
-				'channels': numChannels,
-				'sampleRate': 48000
-			});
+		for(let i=0; i<data.length; i+=2) {
+			let channel = i / 2 % numChannels,
+				sample = i / numChannels - channel;
 
-			return;
-		} else {
-			speaker.write(data);
+			// console.log('Reading the', sample + 'th sample of channel', channel);
+
+			audioData.writeInt16LE(channels[channel].readInt16LE(sample), i);
 		}
-	});
+
+		speaker.write(audioData);
+	}).on('close', () => 
+		console.log('Client disconnected') );
 });
